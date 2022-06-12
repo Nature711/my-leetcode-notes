ae0a900d5111984c13cb2838140368f944e35758
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2022-06-12 14:56:38 +0800

    Time: 182 ms (22.39%), Space: 67.5 MB (60.93%) - LeetHub

--- 55-jump-game/55-jump-game.java
+++ 55-jump-game/55-jump-game.java
@@ -2,27 +2,17 @@ class Solution {
     public boolean canJump(int[] nums) {
         int n = nums.length;
         boolean[] dp = new boolean[n];
-        // dp[0] = true;
+        dp[0] = true;
         
-        // for (int i = 1; i < n; i++) {
-        //     for (int j = 0; j < i; j++) {
-        //         if (dp[j] && (nums[j] >= i - j)) {
-        //             dp[i] = true;
-        //             break;
-        //         }
-        //     }
-        // }
-        
-        dp[n - 1] = true;
-        
-        for (int i = n - 2; i >= 0; i--) {
-            for (int j = 1; j <= nums[i]; j++) {
-                if (i + j < n && dp[i + j]) dp[i] = true;
+        for (int i = 0; i < n; i++) {
+            int min = nums[i] == 0 ? -1 : 0;
+            if (dp[i] == false) break;
+            for (int j = nums[i]; j > min; j--) {
+                if (i + j == n - 1) return true;
+                if (i + j < n) dp[i + j] = true;
             }
         }
-        
-        
-        return dp[0];
+        return dp[n - 1];
     }
     
 }
