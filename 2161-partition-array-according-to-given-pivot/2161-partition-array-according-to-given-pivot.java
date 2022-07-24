87659f7a1284f99a8015fea15392fcf0e040796f
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2022-07-24 10:57:48 +0800

    Time: 7 ms (85.52%), Space: 168.5 MB (41.72%) - LeetHub

--- 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
+++ 2161-partition-array-according-to-given-pivot/2161-partition-array-according-to-given-pivot.java
@@ -1,37 +1,18 @@
 class Solution {
     public int[] pivotArray(int[] nums, int pivot) {
-        int len = nums.length;
+        int n = nums.length;
         
-        List<Integer> small = new ArrayList<>();
-        List<Integer> large = new ArrayList<>();
-        int pivotCount = 0;
+        int left = 0;
+        int right = n - 1;
+        int[] ans = new int[n];
         
-        for (int i = 0; i < len; i++) {
-            if (nums[i] == pivot) {
-                pivotCount++;
-            } else if (nums[i] < pivot) {
-                small.add(nums[i]);
-            } else {
-                large.add(nums[i]);
-            }
+        for (int i = 0, j = n - 1; i < n; i++, j--) {
+            if (nums[i] < pivot) ans[left++] = nums[i];
+            if (nums[j] > pivot) ans[right--] = nums[j];
         }
         
-        int i = 0;
-        for (int j = 0; j < small.size(); j++) {
-            nums[i] = small.get(j);
-            i++;
-        }
+        while (left <= right) ans[left++] = pivot;
         
-        for (int j = 0; j < pivotCount; j++) {
-            nums[i] = pivot;
-            i++;
-        }
-        
-        for (int j = 0; j < large.size(); j++) {
-            nums[i] = large.get(j);
-            i++;
-        }
-        
-        return nums;
+        return ans;
     }
 }
