36dd3af55bdb77675bda702316f33dec7b2548a5
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2022-07-30 00:23:39 +0800

    Time: 0 ms (100.00%), Space: 42.1 MB (11.60%) - LeetHub

--- 100-same-tree/100-same-tree.java
+++ 100-same-tree/100-same-tree.java
@@ -16,13 +16,27 @@
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         
-        if (p == null && q == null) return true;
-        if ((p == null && q != null) || (p != null && q == null)) return false;
+        Queue<TreeNode> q1 = new LinkedList<>();
+        Queue<TreeNode> q2 = new LinkedList<>();
+        q1.offer(p);
+        q2.offer(q);
         
-        if (p.val == q.val) {
-            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
-        } else {
-            return false;
+        while (!q1.isEmpty() && !q2.isEmpty()) {
+            TreeNode t1 = q1.poll();
+            TreeNode t2 = q2.poll();
+            
+            if (t1 == null && t2 == null) continue;
+            if ((t1 == null && t2 != null) || (t1 != null && t2 == null)) return false;
+            
+            if (t1.val != t2.val) return false;
+            
+            q1.offer(t1.left);
+            q1.offer(t1.right);
+            q2.offer(t2.left);
+            q2.offer(t2.right);
         }
+        
+        if (q1.isEmpty() && q2.isEmpty()) return true;
+        else return false;
     }
 }
