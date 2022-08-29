f9096d7dfe0e9c5194a73f3097e87cdfbaf9261b
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2022-08-29 13:27:03 +0800

    Time: 12 ms (90.55%), Space: 44.5 MB (98.60%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.java
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.java
@@ -1,39 +1,20 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         
-        Map<Integer, Integer> map = new HashMap<>();
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for (int num: nums) map.put(num, map.getOrDefault(num, 0) + 1);
         
-        for (int num: nums) {
-            if (map.containsKey(num)) map.put(num, map.get(num) + 1);
-            else map.put(num, 1);
-        }
+        PriorityQueue<Integer> pq = new PriorityQueue<>((x,y) -> map.get(x) - map.get(y)); //min heap
         
-        Map<Integer, List<Integer>> frequencyTable = new HashMap<>();
-        
-        for (int num: map.keySet()) {
-            int frequency = map.get(num);
-            if (frequencyTable.containsKey(frequency)) {
-                frequencyTable.get(frequency).add(num);
-            } else {
-                List<Integer> arr = new ArrayList<>();
-                arr.add(num);
-                frequencyTable.put(frequency, arr);
-            }
+        for(int num: map.keySet()) {
+            pq.add(num);
+            if (pq.size() > k) pq.poll();
         }
         
         int[] res = new int[k];
-        
-        for (int freq = nums.length; freq > 0; freq--) {
-            if (frequencyTable.containsKey(freq)) {
-                List<Integer> list = frequencyTable.get(freq);
-                for (int num: list) {
-                    res[--k] = num;
-                    if (k == 0) return res;
-                }
-            }
+        for (int i = 0; i < k; i++) {
+            res[i] = pq.poll();
         }
-        
         return res;
-            
     }
 }
