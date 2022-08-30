4ceba8a7065785fde99da585a62627b8d6f2bee9
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2022-08-30 11:14:47 +0800

    Time: 2043 ms (13.54%), Space: 381.4 MB (14.50%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.java
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.java
@@ -1,27 +1,44 @@
 class Solution {
     public String frequencySort(String s) {
+        
         HashMap<Character, Integer> map = new HashMap<>();
         
         for (int i = 0; i < s.length(); i++) {
             map.put(s.charAt(i), map.getOrDefault(s.charAt(i), 0) + 1);
         }
         
-        PriorityQueue<Pair<Character, Integer>> pq = new PriorityQueue<>((p1, p2) -> p2.getValue() - p1.getValue());
+        
+        HashMap<Integer, List<Character>> buckets = new HashMap<>();
         
         for (char c: map.keySet()) {
-            pq.add(new Pair<>(c, map.get(c)));
-        }
-        
-        String res = "";
-        while (!pq.isEmpty()) {
-            Pair<Character, Integer> currP = pq.poll();
-            int freq = currP.getValue();
-            char c = currP.getKey();
-            while (freq > 0) {
-                res += c;
-                freq--;
+            int freq = map.get(c);
+            if (buckets.containsKey(freq)) {
+                buckets.get(freq).add(c);
+            } else {
+                List<Character> list = new ArrayList<>();
+                list.add(c);
+                buckets.put(freq, list);
             }
         }
+     
+        String res = "";
+        
+        for (int freq = s.length(); freq > 0; freq--) {
+            if (buckets.containsKey(freq)) {
+                List<Character> list = buckets.get(freq);
+                for (int i = 0; i < list.size(); i++) {
+                    if (res.length() == s.length()) return res;
+                    int f = freq;
+                    char c = list.get(i);
+                    while (f > 0) {
+                        res += c;
+                        f--;
+                    }
+                }
+            }
+            if (res.length() == s.length()) return res;
+        }
+     
         return res;
         
     }
