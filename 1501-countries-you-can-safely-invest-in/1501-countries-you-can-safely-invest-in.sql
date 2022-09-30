c83d3fcbabb7c77ca752f8b44d9d76f838219872
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2022-09-30 19:21:44 +0800

    Time: 3153 ms (5.03%), Space: 0B (100.00%) - LeetHub

---
+++ 1501-countries-you-can-safely-invest-in/1501-countries-you-can-safely-invest-in.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+# for each country, find the average call duration
+with person_country as (
+    select id, SUBSTRING(phone_number, 1, 3) as phone_code
+    from Person
+),
+country_duration as (
+    select ct.name as country, AVG(c.duration) as country_avg_duration
+    from person_country pc, Country ct, Calls c
+    where (pc.id = c.caller_id or pc.id = c.callee_id) and pc.phone_code = ct.country_code
+    group by country_code
+)
+select country
+from country_duration 
+where country_avg_duration > (select avg(duration) as global_avg_duration from Calls);
+
