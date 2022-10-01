3cd13bac41955cbf8da62eddd8cecb0b7da67945
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2022-10-01 23:15:33 +0800

    Time: 2 ms (33.70%), Space: 40.7 MB (66.35%) - LeetHub

--- 343-integer-break/343-integer-break.java
+++ 343-integer-break/343-integer-break.java
@@ -1,18 +1,17 @@
 class Solution {
     
-   HashMap<Integer, Integer> memo = new HashMap<>();
-    
     public int integerBreak(int n) {
-        if (n <= 2) return 1;
+        int[] dp = new int[n + 1];
         
-        int res = 1;
-        for (int curr = n - 1; curr > 0; curr--) {
-            if (!memo.containsKey(n - curr)) {
-                memo.put(n - curr, integerBreak(n - curr));
+        dp[1] = 1;
+        
+        for (int curr = 2; curr <= n; curr++) {
+            for (int i = curr - 1; i > 0; i--) {
+                dp[curr] = Math.max(dp[curr], Math.max(i * dp[curr - i], i * (curr - i)));
             }
-            res = Math.max(res, Math.max(curr * memo.get(n - curr), curr * (n - curr)));
         }
-        return res;
+        
+        return dp[n];
     }
 }
 
