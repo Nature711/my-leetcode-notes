820ccc392d276dd7284280f0567c42a7ec6dc034
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2022-10-02 23:55:16 +0800

    Time: 422 ms (56.40%), Space: 0B (100.00%) - LeetHub

--- 182-duplicate-emails/182-duplicate-emails.sql
+++ 182-duplicate-emails/182-duplicate-emails.sql
@@ -1,4 +1,6 @@
 # Write your MySQL query statement below
 
-select email from Person group by email having count(*) > 1;
+# select email from Person group by email having count(*) > 1;
+
+select distinct p1.email from Person p1, Person p2 where p1.id <> p2.id and p1.email = p2.email;
  
