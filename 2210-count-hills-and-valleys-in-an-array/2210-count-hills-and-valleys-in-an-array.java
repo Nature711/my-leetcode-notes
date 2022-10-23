9fbee2cf05c0ecf097e6dd02322bca92ae084bdd
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2022-10-23 14:12:57 +0800

    Time: 1 ms (82.02%), Space: 42.6 MB (12.59%) - LeetHub

--- 2210-count-hills-and-valleys-in-an-array/2210-count-hills-and-valleys-in-an-array.java
+++ 2210-count-hills-and-valleys-in-an-array/2210-count-hills-and-valleys-in-an-array.java
@@ -2,27 +2,29 @@ class Solution {
     public int countHillValley(int[] nums) {
         int count = 0;
         int i = 1;
+        int left = nums[0];
         
         while (i < nums.length - 1) {
-            if ((nums[i] > nums[i - 1] && nums[i] > nums[i + 1]) 
-                || (nums[i] < nums[i - 1] && nums[i] < nums[i + 1])) {
+            if ((nums[i] > left && nums[i] > nums[i + 1]) 
+                || (nums[i] < left && nums[i] < nums[i + 1])) {
                 //is hill or is valley
+                left = nums[i];
                 count++;
 
                 i++;
-            } else if (nums[i] == nums[i + 1]) {
-                int prev = nums[i - 1];
-                int start = nums[i];
-                while (i < nums.length && nums[i] == start) {
+//             } else if (nums[i] == nums[i + 1]) {
+//                 int prev = nums[i - 1];
+//                 int start = nums[i];
+//                 while (i < nums.length && nums[i] == start) {
      
-                    i++;
-                }
-                if (i == nums.length) break;
-                if ((prev > start && nums[i] > start) ||
-                    (prev < start && nums[i] < start)) {
-                    count++;
+//                     i++;
+//                 }
+//                 if (i == nums.length) break;
+//                 if ((prev > start && nums[i] > start) ||
+//                     (prev < start && nums[i] < start)) {
+//                     count++;
    
-                }
+//                 }
             } else i++;
         }
         
