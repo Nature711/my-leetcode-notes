62593581cf9edc91d3e1c9f03ed5ee53c70ed00a
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2022-10-28 11:20:42 +0800

    Time: 16 ms (6.48%), Space: 51.8 MB (87.41%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,47 +1,26 @@
 class Solution {
-    private int[] numsArray;
-    
+    int[] numss;
     public int maxSubArray(int[] nums) {
-        numsArray = nums;
-        
-        // Our helper function is designed to solve this problem for
-        // any array - so just call it using the entire input!
-        return findBestSubarray(0, numsArray.length - 1);
+        numss = nums;
+        return solver(0, nums.length - 1);
     }
     
-    private int findBestSubarray(int left, int right) {
-        // Base case - empty array.
-        if (left > right) {
-            return Integer.MIN_VALUE;
+    public int solver(int low, int high) {
+        if (high < low) return Integer.MIN_VALUE;
+        
+        int mid = Math.floorDiv(low + high, 2);
+       
+        int leftMax = 0, leftSum = 0, rightMax = 0, rightSum = 0;
+        for (int i = mid - 1; i >= low; i--) {
+            leftSum += numss[i];
+            leftMax = Math.max(leftMax, leftSum);
         }
-        
-        int mid = Math.floorDiv(left + right, 2);
-        int curr = 0;
-        int bestLeftSum = 0;
-        int bestRightSum = 0;
-        
-        // Iterate from the middle to the beginning.
-        for (int i = mid - 1; i >= left; i--) {
-            curr += numsArray[i];
-            bestLeftSum = Math.max(bestLeftSum, curr);
+        for (int i = mid + 1; i <= high; i++) {
+            rightSum += numss[i];
+            rightMax = Math.max(rightMax, rightSum);
         }
-        
-        // Reset curr and iterate from the middle to the end.
-        curr = 0;
-        for (int i = mid + 1; i <= right; i++) {
-            curr += numsArray[i];
-            bestRightSum = Math.max(bestRightSum, curr);
-        }
-        
-        // The bestCombinedSum uses the middle element and the best
-        // possible sum from each half.
-        int bestCombinedSum = numsArray[mid] + bestLeftSum + bestRightSum;
-        
-        // Find the best subarray possible from both halves.
-        int leftHalf = findBestSubarray(left, mid - 1);
-        int rightHalf = findBestSubarray(mid + 1, right);
-        
-        // The largest of the 3 is the answer for any given input array.
-        return Math.max(bestCombinedSum, Math.max(leftHalf, rightHalf));
+        int left = solver(low, mid - 1);
+        int right = solver(mid + 1, high);
+        return Math.max((leftMax + rightMax + numss[mid]), Math.max(left, right));
     }
 }
