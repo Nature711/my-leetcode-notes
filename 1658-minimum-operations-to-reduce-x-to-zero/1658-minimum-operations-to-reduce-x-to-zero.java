08e704e74d801cbc10abb1bcd05c97d65618e3b5
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2022-10-29 10:58:22 +0800

    Time: 76 ms (34.65%), Space: 57.6 MB (87.10%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.java
@@ -1,44 +1,26 @@
 class Solution {
     public int minOperations(int[] nums, int x) {
-        int n = nums.length;
-        HashMap<Integer, Integer> forward_sum_map = new HashMap<>();
-        HashMap<Integer, Integer> backward_sum_map = new HashMap<>();
-        int forward_sum = 0;
-        int backward_sum = 0;
-        
-        for (int i = 0; i < n; i++) {
-            forward_sum += nums[i];
-            forward_sum_map.put(forward_sum, i + 1);
+        int sum = 0;
+        for (int num: nums) sum += num;
+        if (sum - x == 0) return nums.length;
+        int max = maxSubArrayLen(nums, sum - x);
+        return max == -1 ? max : nums.length - max;
+    }
+    
+    public int maxSubArrayLen(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int cumuSum = 0;
+        int maxLen = 0;
+        map.put(0, 0);
+        boolean found = false;
+        for (int i = 0; i < nums.length; i++) {
+            cumuSum += nums[i];
+            if (map.containsKey(cumuSum - k)) {
+                found = true;
+                maxLen = Math.max(maxLen, i + 1 - map.get(cumuSum - k));
+            }
+            if (!map.containsKey(cumuSum)) map.put(cumuSum, i + 1);
         }
-        
-        for (int i = n - 1; i >= 0; i--) {
-            backward_sum += nums[i];
-            backward_sum_map.put(backward_sum, nums.length - i);
-        }
-        
-        int minOp = n + 1;
-        
-        for (int f_sum: forward_sum_map.keySet()) {
-            if (backward_sum_map.containsKey(x - f_sum)) {
-                int op = forward_sum_map.get(f_sum) + backward_sum_map.get(x- f_sum);
-                minOp = Math.min(minOp, op);   
-                break;
-            } 
-        }
-        
-        for (int b_sum: backward_sum_map.keySet()) {
-            if (forward_sum_map.containsKey(x - b_sum)) {
-                int op = forward_sum_map.get(x - b_sum) + backward_sum_map.get(b_sum);
-                minOp = Math.min(minOp, op);  
-                break;
-            } 
-        }
-        
-        if (forward_sum_map.containsKey(x)) minOp = Math.min(minOp, forward_sum_map.get(x));
-        
-        if (backward_sum_map.containsKey(x)) minOp = Math.min(minOp, backward_sum_map.get(x));
-        
-        return minOp <= n ? minOp : -1;
-        
+        return found ? maxLen : -1;
     }
 }
