5593fdf66f7c92cfd92a9b86582caead2a01b932
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2022-11-04 23:15:37 +0800

    Time: 10 ms (88.23%), Space: 41.7 MB (89.04%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,7 +1,6 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if (x == 0) return true;
-        if (x < 0) return false;
+        if (x == 0) return true;        if (x < 0) return false;
         
         List<Integer> xList = new ArrayList<>();
         while (x > 0) {
