c5bbebfe77a57aeeec5f739d26cb7d35ab1db265
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2022-11-06 11:56:21 +0800

    Time: 18 ms (35.27%), Space: 48.4 MB (41.93%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.java
@@ -0,0 +1,38 @@
+class Solution {
+    int[] states;
+    HashMap<Integer, List<Integer>> map = new HashMap<>();
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        states = new int[numCourses]; //0 for not explored, 1 for exploroing, 2 for explored
+        
+        for (int[] prerequisite: prerequisites) {
+            if (!map.containsKey(prerequisite[0])) map.put(prerequisite[0], new ArrayList<>());
+            map.get(prerequisite[0]).add(prerequisite[1]);
+        }
+        
+        for (int i = 0; i < numCourses; i++) {
+            if (states[i] == 0) {
+                if (!dfs(i)) return false;
+            }
+        }
+        
+        return true;
+    }
+    
+    
+    public boolean dfs(int start) {
+        
+        if (states[start] == 2) return true;
+        // if (states[start] == 1) return false; //find cycle
+        if (!map.containsKey(start)) return true; //no dependency
+        
+        states[start] = 1; //exploring
+         
+        for (int course: map.get(start)) {
+            if (states[course] == 1) return false;
+            if (!dfs(course)) return false;
+        }
+        
+        states[start] = 2; //explored
+        return true;
+    }
+} 
