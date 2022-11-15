ebea3603f633f98e9cf2981d1329ec9358fd3a8b
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2022-11-15 11:13:32 +0800

    Time: 0 ms (100.00%), Space: 49.6 MB (63.82%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -14,48 +14,25 @@
  * }
  */
 class Solution {
+    
+    public int countDepth(TreeNode root) {
+        int depth = 0;
+        while (root != null) {
+            root = root.left;
+            depth++;
+        }
+        return depth;
+    }
 
     public int countNodes(TreeNode root) {
         if (root == null) return 0;
- 
-        TreeNode curr = root;
-        int depth = 0;
         
-        while (curr.left != null) {
-            curr = curr.left;
-            depth++;
-        }
-  
+        int leftDepth = countDepth(root.left);
+        int rightDepth = countDepth(root.right);
         
-        int low = 1, high = (int) Math.pow(2, depth) - 1;
-        
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (exists(mid, root, depth)) low = mid + 1;
-            else high = mid - 1;
-        }
-
-        return (int) Math.pow(2, depth) - 1 + low;
-        
-    }
-    
-    // returns true if the last node at idx exists in the last row
-    public boolean exists(int idx, TreeNode curr, int depth) {
-        
-        int low = 0, high = (int) Math.pow(2, depth) - 1;
-        for (int i = 0; i < depth; i++) {
-
-            int mid = low + (high - low) / 2;
-            if (idx <= mid) {
-                curr = curr.left;
-                high = mid;
-            } else {
-                curr = curr.right;
-                low = mid + 1;
-            }
-        }
-        return curr != null;
-        
+        if (leftDepth == rightDepth + 1) 
+            return countNodes(root.left) + (int)Math.pow(2, rightDepth);
+        else return countNodes(root.right) + (int)Math.pow(2, leftDepth);
     }
     
     
