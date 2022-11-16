2fa8269bfbb6254651d316d201744fbee631e9fe
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2022-11-16 10:54:15 +0800

    Time: 0 ms (100.00%), Space: 44.9 MB (94.47%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -15,10 +15,10 @@
  */
 class Solution {
     
-    public int countDepth(TreeNode root) {
+    public int countDepth(TreeNode root, boolean goLeft) {
         int depth = 0;
         while (root != null) {
-            root = root.left;
+            root = goLeft ? root.left : root.right;
             depth++;
         }
         return depth;
@@ -27,12 +27,31 @@ class Solution {
     public int countNodes(TreeNode root) {
         if (root == null) return 0;
         
-        int leftDepth = countDepth(root.left);
-        int rightDepth = countDepth(root.right);
+        int maxDepth = countDepth(root, true);
+        int rightDepth = countDepth(root, false);
         
-        if (leftDepth == rightDepth + 1) 
-            return countNodes(root.left) + (int)Math.pow(2, rightDepth);
-        else return countNodes(root.right) + (int)Math.pow(2, leftDepth);
+        if (maxDepth == rightDepth) return (int) Math.pow(2, maxDepth) - 1;
+        
+        int numLeaf = (int) Math.pow(2, maxDepth - 1) - 1;
+        TreeNode leftTree = root.left, rightTree = root.right;
+        int leftRightDepth = countDepth(leftTree, false), leftDepth = maxDepth - 1;
+        
+        while (leftTree != null) {  
+            if (leftDepth > leftRightDepth) {
+                rightTree = leftTree.right;
+                leftTree = leftTree.left;
+            }
+            else {
+                numLeaf += (int) Math.pow(2, leftDepth - 1);
+                if (rightTree == null) break;
+                leftTree = rightTree.left;
+                rightTree = rightTree.right;
+            }
+            leftRightDepth = countDepth(leftTree, false);
+            leftDepth--;
+        }
+        
+        return numLeaf;
     }
     
     
