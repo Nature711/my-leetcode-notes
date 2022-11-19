cb3ce261a4a3ca13595fe1b89a240bacb3ee442c
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2022-11-19 10:25:38 +0800

    Time: 23 ms (5.07%), Space: 49.2 MB (5.85%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.java
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.java
@@ -16,10 +16,23 @@
 class Solution {
     List<Integer> nodes = new ArrayList<>();
     int idx = 0;
+    
     public void recoverTree(TreeNode root) {
-        inorder(root);
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode curr = root;
+        
+        while (curr != null || !stack.isEmpty()) {
+            while (curr != null) {
+                stack.push(curr);
+                curr = curr.left;
+            }
+            TreeNode popped = stack.pop();
+            nodes.add(popped.val);
+            curr = popped.right;
+        }
+   
         Collections.sort(nodes);
-        // System.out.println(nodes);
+        System.out.println(nodes);
         inorder2(root);
     }
     
