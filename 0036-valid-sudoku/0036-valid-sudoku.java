aa4db195f91c5dcac12153d3b425f61ff2537d1a
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2022-11-23 09:36:42 +0800

    Time: 6 ms (69.72%), Space: 47.5 MB (42.59%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.java
+++ 0036-valid-sudoku/0036-valid-sudoku.java
@@ -5,16 +5,25 @@ class Solution {
         
         List<HashSet<Character>> sets = new ArrayList<>();
         
-        for (int i = 0; i < 9; i++) sets.add(new HashSet<>());
+        List<HashSet<Character>> boxSets = new ArrayList<>();
+        
+        for (int i = 0; i < 9; i++) {
+            if (i < 3) boxSets.add(new HashSet<>());
+            sets.add(new HashSet<>());
+        }
+        
+        
         
         for (int i = 0; i < 9; i++) {
             for (int j = 0; j < 9; j++) {
                 if (board[i][j] != '.') {
                     if (!set.add(board[i][j])) return false;
                     if (!sets.get(j).add(board[i][j])) return false;
+                    if (!boxSets.get(j / 3).add(board[i][j])) return false;
                 }
             }
-            set = new HashSet<>();
+            set.clear();
+            if (i % 3 == 2) for (int a = 0; a < 3; a++) boxSets.get(a).clear();
         }
         
 //         //column check
@@ -25,16 +34,16 @@ class Solution {
 //             set = new HashSet<>();
 //         }
         
-        //box check
-        for (int i = 0; i < 9; i++) {
-            int startRow  = 3 * (i / 3), startCol = 3 * (i % 3);
-            for (int a = startRow; a < startRow + 3; a++) {
-                for (int b = startCol; b < startCol + 3; b++) {
-                    if (board[a][b] != '.' && !set.add(board[a][b])) return false;
-                }
-            }
-            set = new HashSet<>();
-        }
+//        //box check
+//         for (int i = 0; i < 9; i++) {
+//             int startRow  = 3 * (i / 3), startCol = 3 * (i % 3);
+//             for (int a = startRow; a < startRow + 3; a++) {
+//                 for (int b = startCol; b < startCol + 3; b++) {
+//                     if (board[a][b] != '.' && !set.add(board[a][b])) return false;
+//                 }
+//             }
+//             set = new HashSet<>();
+//         }
         
         return true;
     }
