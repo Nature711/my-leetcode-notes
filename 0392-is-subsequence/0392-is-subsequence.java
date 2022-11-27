27ffd2bc4c50158c4507dfc0301d6f560bbd9308
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2022-11-27 14:12:23 +0800

    Time: 4 ms (37.55%), Space: 42.1 MB (45.71%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -1,19 +1,21 @@
 class Solution {
     public boolean isSubsequence(String s, String t) {
-        
         if (s.length() == 0) return true;
         
-        Stack<Character> stack = new Stack<>();
+        int sptr = 0, tptr = 0;
         
-        for (char c: t.toCharArray()) stack.push(c);
-        
-        int ptr = s.length() - 1;
-        
-        while (!stack.isEmpty()) {
-            if (stack.pop() == s.charAt(ptr)) ptr--;
-            if (ptr < 0) return true;
+        while (sptr < s.length() && tptr < t.length()) {
+            //keep advancing tptr until we find a match with sptr, or tptr is out of bounds
+            while (tptr < t.length() && s.charAt(sptr) != t.charAt(tptr)) tptr++;
+            /**when exiting from the while loop above, it means:
+            //either we find a match in s, in which case we want to advance sptr, or
+            //tptr is out of bounds, in which case we want to break from this 
+            **/
+            tptr++;
+            if (tptr > t.length()) return false;
+            sptr++;
         }
         
-        return false;
+        return sptr == s.length();
     }
 }
