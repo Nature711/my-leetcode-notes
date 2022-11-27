3e84c53a7149b652fd50e73669246766f417b18c
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2022-11-27 17:51:59 +0800

    Time: 0 ms (100.00%), Space: 42.6 MB (65.26%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,16 +1,13 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int[] res = new int[m + n];
-        int i = 0, j = 0, k = 0;
-        while (i < m && j < n) {
-            if (nums1[i] <= nums2[j]) res[k++] = nums1[i++];
-            else res[k++] = nums2[j++];
+        int i = m - 1, j = n - 1, k = m + n - 1;
+        while (i >= 0 && j >= 0) {
+            if (nums1[i] > nums2[j]) nums1[k--] = nums1[i--];
+            else nums1[k--] = nums2[j--];
         }
         
-        while (i < m) res[k++] = nums1[i++];
-        while (j < n) res[k++] = nums2[j++];
-        
-        for (int a = 0; a < m + n; a++) nums1[a] = res[a];
+        while (i >= 0) nums1[k--] = nums1[i--];
+        while (j >= 0) nums1[k--] = nums2[j--];
     
     }
 }
