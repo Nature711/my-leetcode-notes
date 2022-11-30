06048adf181b06a231514f8c5ab54ac6dc5c7f27
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2022-12-01 07:51:06 +0800

    Time: 3 ms (37.83%), Space: 42.7 MB (97.29%) - LeetHub

--- 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.java
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.java
@@ -19,16 +19,20 @@ class Node {
 
 class Solution {
     
-    List<Integer> res = new ArrayList<>();
-    
     public List<Integer> preorder(Node root) {
-        dfs(root);
+        
+        List<Integer> res = new ArrayList<>();
+        if (root == null) return res;
+        
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            Node node = stack.pop();
+            res.add(node.val);
+            for (int i = node.children.size() - 1; i >= 0; i--) stack.push(node.children.get(i));
+        }
         return res;
     }
     
-    public void dfs(Node root) {
-        if (root == null) return;
-        res.add(root.val);
-        for (Node child: root.children) dfs(child);
-    }
+    
 }
