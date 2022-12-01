913f0f51dce0d1a140390427d32e501d4932cf07
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2022-12-01 12:18:48 +0800

    Time: 3 ms (93.01%), Space: 46.3 MB (61.55%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.java
+++ 0383-ransom-note/0383-ransom-note.java
@@ -1,17 +1,9 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        char[] note = ransomNote.toCharArray();
-        char[] mag = magazine.toCharArray();
-        Arrays.sort(note);
-        Arrays.sort(mag);
-        
-        int i = 0, j = 0;
-        while (i < note.length) {
-            if (j == mag.length) return false;
-            if (note[i] == mag[j]) {
-                i++;
-                j++; 
-            } else j++;
+        int[] map = new int[26];
+        for (char c: magazine.toCharArray()) map[c - 'a']++;
+        for (char c: ransomNote.toCharArray()) {
+            if (map[c - 'a']-- == 0) return false;
         }
         return true;
     }
