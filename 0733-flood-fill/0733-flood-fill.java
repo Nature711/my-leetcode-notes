ba35003ad834c287e7fca1bfaac2fed607c9f192
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2022-12-04 09:53:46 +0800

    Time: 4 ms (8.91%), Space: 48.8 MB (7.70%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,26 +1,32 @@
 class Solution {
-    int[][] globalImage;
-    boolean[][] visited;
-    int destColor, srcColor;
-    int m, n;
-    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        globalImage = image;
-        m = image.length;
-        n = image[0].length;
-        visited = new boolean[m][n];
-        destColor = color;
-        srcColor = image[sr][sc];
-        dfs(sr, sc);
-        return globalImage;
-    }
     
-    public void dfs(int r, int c) {
-        if (r < 0 || r == m || c < 0 || c == n || visited[r][c] || globalImage[r][c] != srcColor) return;
-        globalImage[r][c] = destColor;
-        visited[r][c] = true;
-        dfs(r - 1, c);
-        dfs(r + 1, c);
-        dfs(r, c - 1);
-        dfs(r, c + 1);
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int[][] directions = {{0,1},{1,0},{-1,0},{0,-1}};
+        int m = image.length, n = image[0].length, srcColor = image[sr][sc];
+        int[][] res = new int[m][n];
+        boolean[][] visited = new boolean[m][n];
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[] {sr, sc});
+        
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                int[] curr = queue.poll();
+                int r = curr[0];
+                int c = curr[1];
+                image[r][c] = color;
+                
+                for (int[] direction: directions) {
+                    int nextR = r + direction[0];
+                    int nextC = c + direction[1];
+                    if (nextR >= 0 && nextR < m && nextC >= 0 && nextC < n && !visited[nextR][nextC] & image[nextR][nextC] == srcColor) { 
+                        queue.offer(new int[] {nextR, nextC});
+                        visited[nextR][nextC] = true;
+                    }
+                }
+            }
+        }
+        
+        return image;
     }
 }
