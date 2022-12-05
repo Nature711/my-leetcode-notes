2833493a8a7a20fa59145cb736b9de7b00fb3fa3
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2022-12-05 09:16:53 +0800

    Time: 1 ms (49.41%), Space: 41.4 MB (12.94%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,6 +1,10 @@
 class Solution {
+    HashMap<Integer, Integer> memo = new HashMap<>();
     public int fib(int n) {
         if (n == 0 || n == 1) return n;
-        return fib(n - 1) + fib(n - 2);
+        if (memo.containsKey(n)) return memo.get(n);
+        int res = fib(n - 1) + fib(n - 2);
+        memo.put(n, res);
+        return res;
     }
 }
