c165ca75c3ccdf41fc11a1e0e84488fbe78d6640
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2022-12-07 09:59:49 +0800

    Time: 702 ms (9.97%), Space: 43.9 MB (80.43%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -2,10 +2,28 @@ class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         List<Integer> res = new ArrayList<>();
         int m = s.length(), n = p.length();
-        for (int i = 0; i <= m - n; i++) {
-            if (isAnagram(s.substring(i, i + n), p)) res.add(i);
+    
+        int i = 0; 
+        while (i <= m - n) {
+            while (i <= m - n) {
+                if (isAnagram(s.substring(i, i + n), p)) {
+                    res.add(i);
+                    break;
+                }
+                i++;
+            }
+            //i is the starting index of the first anagram
+            //first angaram window: [i, i + n)
+            while (i < m - n && s.charAt(i + n) == s.charAt(i)) {
+                res.add(i + 1);
+                i++;
+            }
+            i++;
         }
+        
+        
         return res;
+        
     }
     
     public boolean isAnagram(String s, String t) {
