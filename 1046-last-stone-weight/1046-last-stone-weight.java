ae276b09254a3b92ba57c4130b8e07d392c235e6
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2022-12-10 12:06:19 +0800

    Time: 2 ms (80.33%), Space: 41.5 MB (50.08%) - LeetHub

--- 1046-last-stone-weight/1046-last-stone-weight.java
+++ 1046-last-stone-weight/1046-last-stone-weight.java
@@ -1,21 +1,15 @@
 class Solution {
     public int lastStoneWeight(int[] stones) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
         
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> (b - a));
         for (int stone: stones) pq.add(stone);
         
-        int x = 0;
-        int y = 0;
-        
         while (pq.size() >= 2) {
-            y = pq.remove();
-            x = pq.remove();
-            if (x == y) continue;
-            else pq.add(y - x);
+            int y = pq.poll();
+            int x = pq.poll();
+            if (x != y) pq.add(y - x);
         }
         
-        if (pq.size() == 0) return 0;
-        else return pq.remove();
-        
+        return pq.size() == 0 ? 0 : pq.poll();
     }
 }
