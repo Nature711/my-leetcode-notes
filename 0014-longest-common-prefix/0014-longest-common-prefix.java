cde718bde4be21d81e2024b6ec587bcd1ea355bc
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2022-12-13 23:33:20 +0800

    Time: 1 ms (92.96%), Space: 40.6 MB (85.09%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.java
+++ 0014-longest-common-prefix/0014-longest-common-prefix.java
@@ -1,13 +1,68 @@
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        int i = 0;
-        while (i < strs[0].length()) {
-            int benchmark = strs[0].charAt(i);
-            for (int j = 1; j < strs.length; j++) {
-                if (i == strs[j].length() || strs[j].charAt(i) != benchmark) return strs[0].substring(0, i);
-            }
-            i++;
+        Trie trie = new Trie();
+        for (String str: strs) {
+            if (str.length() == 0) return "";
+            trie.insert(str);
         }
-        return strs[0];
+        return trie.findLCP();
     }
+    
+    
+    
+    
+    
+    class Node {
+        char c;
+        Node[] children = new Node[27];
+        boolean isTerminating = false;
+
+        public Node() {this.c = 'R';}
+        public Node(char c) {this.c = c;}
+    }
+    
+    class Trie {
+        Node root;
+
+        public Trie() { this.root = new Node(); }
+
+        public void insert(String word) {
+            Node currNode = root;
+            for (int i = 0; i < word.length(); i++) {
+                if (currNode.children[word.charAt(i) - 'a'] == null) {
+                    currNode.children[word.charAt(i) - 'a'] = new Node(word.charAt(i));
+                } 
+                currNode = currNode.children[word.charAt(i) - 'a']; 
+                if (i == word.length() - 1) currNode.isTerminating = true;
+            }
+        }
+        
+        public String findLCP() {
+            StringBuilder sb = new StringBuilder();
+            Node currNode = root;
+            while (currNode != null) {
+                int count = 0;
+                int next = -1;
+                char c = 'R';
+                int flag = 0;
+                for (int i = 0; i < 26; i++) {
+                    if (currNode.children[i] != null) {
+                        next = i;
+                        c = (char) (i + 97);
+                        count++;
+                        if (currNode.children[i].isTerminating) flag = 1;
+                    }
+                    if (count > 1) return sb.toString();
+                }
+                 
+                if (count == 0) return sb.toString();
+                else sb.append(c);
+                if (flag == 1) break;
+                currNode = currNode.children[next];
+            }
+            return sb.toString();
+        }
+    }
+    
+    
 }
