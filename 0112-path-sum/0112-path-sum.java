23e927937e2c9c285fe17a5bf1f0fdd00669a9bd
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2022-12-18 11:51:16 +0800

    Time: 1 ms (69.44%), Space: 43.4 MB (65.89%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -16,16 +16,13 @@
 class Solution {
     
     public boolean hasPathSum(TreeNode root, int targetSum) {
+        
         if (root == null) return false;
-        return dfs(root, 0, targetSum);
-    }
-    
-    public boolean dfs(TreeNode node, int currSum, int targetSum) {
-        currSum += node.val;
-        if (node.left == null && node.right == null && currSum == targetSum) return true;
-        if (node.left != null && dfs(node.left, currSum, targetSum)) return true;
-        if (node.right != null && dfs(node.right, currSum, targetSum)) return true;
-        return false;
+        if (root.left == null && root.right == null) return root.val == targetSum;
+        
+        boolean fromLeft = hasPathSum(root.left, targetSum - root.val);
+        if (fromLeft) return true;
+        else return hasPathSum(root.right, targetSum - root.val);
     }
 }
 
