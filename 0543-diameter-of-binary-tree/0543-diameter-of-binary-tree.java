909882341ef80871e2374c113b3f5d77e5057573
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2022-12-18 10:27:56 +0800

    Time: 1 ms (77.99%), Space: 43.9 MB (8.82%) - LeetHub

--- 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
+++ 0543-diameter-of-binary-tree/0543-diameter-of-binary-tree.java
@@ -14,19 +14,18 @@
  * }
  */
 class Solution {
-    
     int maxDiameter = 0;
     public int diameterOfBinaryTree(TreeNode root) {
+        if (root == null) return 0;
         dfs(root);
         return maxDiameter;
     }
     
     public int dfs(TreeNode root) {
         if (root == null) return 0;
-        int left = dfs(root.left);
-        int right = dfs(root.right);
+        int left = dfs(root.left), right = dfs(root.right);
         maxDiameter = Math.max(maxDiameter, left + right);
-        return Math.max(left, right) + 1;
+        return 1 + Math.max(left, right);
     }
     
 }
