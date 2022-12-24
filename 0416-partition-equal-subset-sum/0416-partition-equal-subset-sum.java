d6c2d4e3ff98252ba60296a8b948f973b08d090d
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2022-12-24 10:35:17 +0800

    Time: 48 ms (74.92%), Space: 43.8 MB (69.10%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,26 +1,27 @@
 class Solution {
+
     public boolean canPartition(int[] nums) {
+
         int n = nums.length;
         int sum = 0;
         for (int num: nums) sum += num;
-        if (sum % 2 != 0) return false;
+        if (sum % 2 == 1) return false;
+        int target = sum / 2;
         
-        int targetSum = sum / 2;
-        boolean[][] dp = new boolean[n + 1][targetSum + 1]; //dp[i][j] = if we can sum up to target j using nums from idx 0 to i
-        
-        dp[0][0] = true;
+        boolean[][] dp = new boolean[n + 1][target + 1]; 
+        //dp[i][j] = whether we can form a subset sum of j using elements from 0...i
+        for (int i = 0; i <= target; i++) dp[0][i] = false;
+        for (int i = 0; i <= n; i++) dp[i][0] = true;
         
         for (int i = 1; i <= n; i++) {
-            for (int j = 0; j <= targetSum; j++) {
-                if (j - nums[i - 1] >= 0) {
-                    dp[i][j] = dp[i - 1][j - nums[i - 1]] || dp[i - 1][j];
-                } else {
-                    dp[i][j] = dp[i - 1][j];
-                }
+            for (int j = 1; j <= target; j++) {
+                int rem = j - nums[i - 1];
+                dp[i][j] = dp[i - 1][j] || (rem < 0 ? false : dp[i - 1][rem]);
+       
             }
         }
-        
-        return dp[n][targetSum];
-        
+  
+        return dp[n][target];
     }
+    
 }
