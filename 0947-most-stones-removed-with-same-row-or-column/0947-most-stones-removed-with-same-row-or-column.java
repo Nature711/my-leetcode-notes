46a6973fa771b9da550970ee7768c2f8690b4a5d
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2023-01-06 17:45:08 +0800

    Time: 81 ms (39.37%), Space: 54 MB (30.35%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
@@ -1,81 +1,48 @@
 class Solution {
    
+    HashSet<Integer> visited = new HashSet<>();
+    List<List<Integer>> graph = new ArrayList();
+    
     public int removeStones(int[][] stones) {
-        
         int n = stones.length;
+        int count = 0;
         
-        UnionFind uf = new UnionFind(n);
-        
-        Arrays.sort(stones, (s1, s2) -> {
-            int diff = s1[0] - s2[0];
-            return diff == 0 ? s1[1] - s2[1] : diff;
-        });
-        
-        int low = stones[0][0], high = stones[n - 1][1];
-        
+        for (int i = 0; i < n; i++) graph.add(new ArrayList<>());
+     
         for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                if (i != j && 
-                    (stones[i][0] == stones[j][0] //share the same row
-                    || stones[i][1] == stones[j][1])) //share the same column
-                    uf.union(i, j);
-                    // System.out.println(uf.numOfComponets);
+            for (int j = i + 1; j < n; j++) {
+                if (i != j && (stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1])) {
+                    graph.get(i).add(j);
+                    graph.get(j).add(i);
+                }
             }
         }
         
-        return n - uf.numOfComponets;
+        System.out.println(graph);
+        
+        for (int i = 0; i < n; i++) {
+            if (!visited.contains(i)) {
+                //System.out.println("dfs from " + i + " called from main");
+                dfs(i);
+                count++;
+            }
+        }
+        
+        return n - count;
     }
     
+    public void dfs(int start) {
+        
+        visited.add(start);
+        List<Integer> neighbors = graph.get(start);
+        
+        for (int neighbor: neighbors) {
+            if (!visited.contains(neighbor)) {
+                //System.out.println("dfs from " + neighbor + " called from recursive");
+                dfs(neighbor);
+                
+            }
+        }
+    }
 }
 
-public class UnionFind {
-    private int[] parents;
-    private int[] size;
-    int numOfComponets = 0;
-
-    public UnionFind(int n) {
-        parents = new int[n];
-        size = new int[n];
-        numOfComponets = n;
-        for (int i = 0; i < parents.length; i++) {
-            parents[i] = i;
-            size[i] = 1;
-        }
-    }
-
-    public int find(int cur) {
-        int root = cur;
-        while (root != parents[root]) {
-            root = parents[root];
-        }
-        // Path Compression
-        while (cur != root) {
-            int preParent = parents[cur];
-            parents[cur] = root;
-            cur = preParent;
-        }
-        return root;
-    }
-
-    public int findComponentSize(int cur) {
-        int parent = find(cur);
-        return size[parent];
-    }
-
-    public void union(int node1, int node2) {
-        int node1Parent = find(node1);
-        int node2Parent = find(node2);
-
-        if (node1Parent == node2Parent)
-            return;
-
-        if (size[node1Parent] > size[node2Parent]) {
-            parents[node2Parent] = node1Parent;
-            size[node1Parent] += size[node2Parent];
-        } else {
-            parents[node1Parent] = node2Parent;
-            size[node2Parent] += size[node1Parent];
-        }
-        numOfComponets--;
-    }
-}
