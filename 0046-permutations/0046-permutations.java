b8c07c5a65a047dd7cc4286ab891c1a59cd9e6ae
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2023-01-07 09:42:38 +0800

    Time: 1 ms (95.06%), Space: 42.8 MB (67.17%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,24 +1,22 @@
 class Solution {
     List<List<Integer>> res = new ArrayList<>();
     public List<List<Integer>> permute(int[] nums) {
-        backtrack(nums, new ArrayList<>(), new HashSet<>());
+        backtrack(nums, new ArrayList<>());
         return res;
     }
     
     
-    public void backtrack(int[] nums, List<Integer> currPerm, HashSet<Integer> used) {
+    public void backtrack(int[] nums, List<Integer> currPerm) {
         if (currPerm.size() == nums.length) {
             res.add(new ArrayList<>(currPerm));
             return;
         }
         
         for (int i = 0; i < nums.length; i++) {
-            if (used.contains(i)) continue;
+            if (currPerm.contains(nums[i])) continue;
             currPerm.add(nums[i]);
-            used.add(i);
-            backtrack(nums, currPerm, used);
+            backtrack(nums, currPerm);
             currPerm.remove(currPerm.size() - 1);
-            used.remove(i);
         }
     }
 }
