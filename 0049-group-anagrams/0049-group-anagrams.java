7f7b2c1fced5f98c3d41e6d3b54ffc967b1ddab5
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2023-01-10 21:27:28 +0800

    Time: 7 ms (89.63%), Space: 45.8 MB (74.10%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,23 +1,26 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        int n = strs.length;
-        HashMap<String, List<Integer>> map = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            char[] arr = strs[i].toCharArray();
-            Arrays.sort(arr);
-            String str = new String(arr);
-            List<Integer> list = map.getOrDefault(str, new ArrayList<>());
-            list.add(i);
-            map.put(str, list);
-        }
-        List<List<String>> result = new ArrayList<>();
-        for (String str: map.keySet()) {
-            List<Integer> indices = map.get(str);
-            List<String> res = new ArrayList<>();
-            for (int idx: indices) res.add(strs[idx]);
-            result.add(res);
+        String[] sorted = new String[strs.length];
+    
+        for (int i = 0; i < strs.length; i++) {
+            char[] chars = strs[i].toCharArray();
+            Arrays.sort(chars);
+            sorted[i] = new String(chars);
         }
         
-        return result;
+        HashMap<String, List<String>> buckets = new HashMap<>();
+        
+        for (int i = 0; i < strs.length; i++) {
+            if (buckets.containsKey(sorted[i])) buckets.get(sorted[i]).add(strs[i]);
+            else {
+                List<String> bucket = new ArrayList<>();
+                bucket.add(strs[i]);
+                buckets.put(sorted[i], bucket);
+            }
+        }
+        
+        List<List<String>> res = new ArrayList<>();
+        for (String key: buckets.keySet()) res.add(buckets.get(key));
+        return res;
     }
 }
