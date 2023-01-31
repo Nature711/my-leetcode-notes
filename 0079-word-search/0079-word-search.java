09223677216b41f7c5379fc26b431fb24ce0a841
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2023-01-31 14:27:23 +0800

    Time: 182 ms (73.56%), Space: 40.4 MB (71.99%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,31 +1,38 @@
 class Solution {
-   boolean[][] visited;
+    char[][] matrix;
+    int m, n;
+    String target;
     public boolean exist(char[][] board, String word) {
-        visited = new boolean[board.length][board[0].length];
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[0].length; j++) {
-                if (dfs(board, i, j, 0, word)) return true;
-            }
+        m = board.length;
+        n = board[0].length;
+        target = word;
+        matrix = board;
+        for (int i = 0; i < m; i++) {
+          for (int j = 0; j < n; j++) {
+              if (board[i][j] == word.charAt(0)) {
+                  boolean res = dfs(i, j, new boolean[m][n], 0);
+                  if (res) return true;
+              }
+          }
         }
         return false;
     }
     
-    
-    public boolean dfs(char[][] board, int i, int j, int wordIdx, String word) {
-        if (wordIdx == word.length()) return true;
-        
-        if (i < 0 || i == board.length || j < 0 || j == board[0].length 
-            || visited[i][j] || word.charAt(wordIdx) != board[i][j]) return false;
+    public boolean dfs(int i, int j, boolean[][] visited, int idx) {
+        if (idx == target.length()) return true;
+        if (i < 0 || i == m || j < 0 || j == n 
+            || visited[i][j] || matrix[i][j] != target.charAt(idx)) return false;
         
         visited[i][j] = true;
         
-        if (dfs(board, i + 1, j, wordIdx + 1, word)) return true;
-        if (dfs(board, i, j + 1, wordIdx + 1, word)) return true;
-        if (dfs(board, i - 1, j, wordIdx + 1, word)) return true;
-        if (dfs(board, i, j - 1, wordIdx + 1, word)) return true;
+        if (dfs(i + 1, j, visited, idx + 1)) return true;
+        if (dfs(i, j + 1, visited, idx + 1)) return true;
+        if (dfs(i - 1, j, visited, idx + 1)) return true;
+        if (dfs(i, j - 1, visited, idx + 1)) return true;
         
         visited[i][j] = false;
         
         return false;
     }
+    
 }
