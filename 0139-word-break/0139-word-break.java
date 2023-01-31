de385e8376f30c84912e5c9556bb3e889e0d94ef
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2023-01-31 13:24:52 +0800

    Time: 417 ms (5.37%), Space: 44.2 MB (23.95%) - LeetHub

---
+++ 0139-word-break/0139-word-break.java
@@ -0,0 +1,27 @@
+class Solution {
+    HashSet<String> dictionary = new HashSet<>();
+    HashMap<String, Boolean> memo = new HashMap<>();
+    
+    public boolean wordBreak(String s, List<String> wordDict) {
+        for (String word: wordDict) dictionary.add(word);
+        return canPartition(s);
+    }
+    
+    public boolean canPartition(String s) {
+        int n = s.length();
+        if (n == 0 || dictionary.contains(s)) return true;
+        
+        if (memo.containsKey(s)) return memo.get(s);
+      
+        boolean flag = false;
+        for (int i = 1; i < n; i++) {
+            if (canPartition(s.substring(0, i))
+                && canPartition(s.substring(i, n))) {
+                flag = true;
+                break;
+            }
+        }
+        memo.put(s, flag);
+        return flag;
+    }
+}
