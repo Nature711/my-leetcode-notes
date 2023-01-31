cf0bd120ae9d26e3942cba6b80708a86c9bcc289
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2023-01-31 14:07:57 +0800

    Time: 7 ms (65.32%), Space: 42.8 MB (37.35%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,27 +1,24 @@
 class Solution {
     HashSet<String> dictionary = new HashSet<>();
-    HashMap<String, Boolean> memo = new HashMap<>();
-    
+ 
     public boolean wordBreak(String s, List<String> wordDict) {
         for (String word: wordDict) dictionary.add(word);
-        return canPartition(s);
+        int n = s.length();
+        boolean[] dp = new boolean[n + 1];
+        //dp[i] = whether the substring s(0...i) inclusive can be partitioned
+        dp[0] = true;
+        
+        for (int i = 0; i <= n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (dp[j] && dictionary.contains(s.substring(j, i))) {
+                    dp[i] = true;
+                    break;
+                }
+            } 
+        }
+        
+        return dp[n];
     }
     
-    public boolean canPartition(String s) {
-        int n = s.length();
-        if (n == 0 || dictionary.contains(s)) return true;
-        
-        if (memo.containsKey(s)) return memo.get(s);
-      
-        boolean flag = false;
-        for (int i = 1; i < n; i++) {
-            if (dictionary.contains(s.substring(0, i))
-                && canPartition(s.substring(i, n))) {
-                flag = true;
-                break;
-            }
-        }
-        memo.put(s, flag);
-        return flag;
-    }
+
 }
