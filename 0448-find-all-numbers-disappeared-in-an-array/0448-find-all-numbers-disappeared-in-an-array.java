b5177aaad6538519dbb52ef48552ebd71170f702
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2023-02-06 17:17:12 +0800

    Time: 6 ms (71.45%), Space: 50 MB (98.98%) - LeetHub

--- 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
+++ 0448-find-all-numbers-disappeared-in-an-array/0448-find-all-numbers-disappeared-in-an-array.java
@@ -1,17 +1,24 @@
 class Solution {
     public List<Integer> findDisappearedNumbers(int[] nums) {
-        int n = nums.length;
-        HashSet<Integer> universe = new HashSet<>();
-        for (int i = 1; i <= n; i++) universe.add(i);
-        HashSet<Integer> seen = new HashSet<>();
-        
-        for (int num: nums) {
-            if (universe.contains(num)) universe.remove(num);
+       
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                //see nums[i] for the first time
+                nums[nums[i] - 1] = - Math.abs(nums[nums[i] - 1]); 
+                //negative marking
+                //take the absolute so that if it's already been marked 
+                //(seen by someone else), we want to leave it negative (marked)
+            } else {
+                //nums[i] itself is negative, meaning it's seen by someone else
+                nums[-nums[i] - 1] = - Math.abs(nums[-nums[i] - 1]);
+            }
         }
         
         List<Integer> res = new ArrayList<>();
-        for (int rem: universe) res.add(rem);
-        return res;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) res.add(i + 1);
+        }
         
+        return res;
     }
 }
