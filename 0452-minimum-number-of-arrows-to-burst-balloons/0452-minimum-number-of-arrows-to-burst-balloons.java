af716a0263887d5ce3d57bfb7a584c79f852f7fe
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2023-02-09 08:58:05 +0800

    Time: 82 ms (78.19%), Space: 80.2 MB (79.79%) - LeetHub

--- 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.java
+++ 0452-minimum-number-of-arrows-to-burst-balloons/0452-minimum-number-of-arrows-to-burst-balloons.java
@@ -3,14 +3,11 @@ class Solution {
         Arrays.sort(points, Comparator.comparingInt(o -> o[1]));
         int arrows = 1;
         long end = points[0][1];
-        // for(int[] point: points) System.out.println("[" + point[0] + "," + point[1] + "]");
-        
-        for (int i = 1; i < points.length; i++) {
-            if (points[i][0] <= end) continue;
-            end = points[i][1];
+        for (int[] balloon: points) {
+            if (balloon[0] <= end) continue;
             arrows++;
+            end = balloon[1];
         }
-        
         return arrows;
     }
     
