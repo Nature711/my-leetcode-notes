857a5d1a863fe3a63d540314c36da7e61ef46687
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2023-02-20 16:30:21 +0800

    Time: 0 ms (100.00%), Space: 42.2 MB (20.97%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.java
@@ -14,27 +14,44 @@ class Node {
 */
 
 class Solution {
+    // public Node copyRandomList(Node head) {
+    //     HashMap<Node, Node> map = new HashMap<>();
+    //     Node ptr = head;
+    //     Node dummy = new Node(0);
+    //     Node curr = dummy;
+    //     while (ptr != null) {
+    //         Node newNode = new Node(ptr.val);
+    //         map.put(ptr, newNode);
+    //         curr.next = newNode;
+    //         ptr = ptr.next;
+    //         curr = curr.next;
+    //     }
+    //     ptr = head;
+    //     curr = dummy.next;
+    //     while (ptr != null) {
+    //         if (ptr.random != null) {
+    //             curr.random = map.get(ptr.random);
+    //         }
+    //         curr = curr.next;
+    //         ptr = ptr.next;
+    //     }
+    //     return dummy.next;
+    // }
+    HashMap<Node, Node> map = new HashMap<>();
+    
     public Node copyRandomList(Node head) {
-        HashMap<Node, Node> map = new HashMap<>();
-        Node ptr = head;
-        Node dummy = new Node(0);
-        Node curr = dummy;
-        while (ptr != null) {
-            Node newNode = new Node(ptr.val);
-            map.put(ptr, newNode);
-            curr.next = newNode;
-            ptr = ptr.next;
-            curr = curr.next;
-        }
-        ptr = head;
-        curr = dummy.next;
-        while (ptr != null) {
-            if (ptr.random != null) {
-                curr.random = map.get(ptr.random);
-            }
-            curr = curr.next;
-            ptr = ptr.next;
-        }
-        return dummy.next;
+        
+        if (head == null) return null;
+        if (map.containsKey(head)) return map.get(head);
+        
+        Node headCopy = new Node(head.val);
+
+        map.put(head, headCopy);
+        
+        Node subproblem = copyRandomList(head.next);
+        headCopy.next = copyRandomList(head.next);
+        headCopy.random = copyRandomList(head.random);
+        
+        return headCopy;
     }
 }
