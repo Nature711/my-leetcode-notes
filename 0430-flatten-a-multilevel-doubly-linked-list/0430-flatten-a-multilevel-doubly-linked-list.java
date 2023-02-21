d302639d04fc61d7d3b27aeed82eb8a79ace9d7d
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2023-02-21 21:53:57 +0800

    Time: 0 ms (100.00%), Space: 40 MB (97.92%) - LeetHub

--- 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.java
+++ 0430-flatten-a-multilevel-doubly-linked-list/0430-flatten-a-multilevel-doubly-linked-list.java
@@ -13,12 +13,14 @@ class Solution {
         if (head == null) return head;
         Node dummy = new Node();
         Node curr = dummy, tmp;
+        //invariant: all the nodes up to and including curr have their child been flatten (i.e., at a single level)
         Stack<Node> stack = new Stack<>();
+        //stack to store the nodes to be processed -- LIFO
         stack.push(head);
         while (!stack.isEmpty()) {
-            tmp = stack.pop();
-            if (tmp.next != null) stack.push(tmp.next);
-            if (tmp.child != null) stack.push(tmp.child);
+            tmp = stack.pop(); 
+            if (tmp.next != null) stack.push(tmp.next); //add node's next first to delay its evaluation -- only after the tmp node's children have been processed
+            if (tmp.child != null) stack.push(tmp.child); //add node's child later which will be processed immediately in the next iteration
             curr.next = tmp;
             tmp.prev = curr;
             tmp.child = null;
