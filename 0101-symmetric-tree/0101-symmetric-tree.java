fb3a15d77e22734973b4405fca99840e81e4ea44
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2023-03-13 14:11:18 +0800

    Time: 0 ms (100.00%), Space: 41.3 MB (19.25%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -22,6 +22,8 @@ class Solution {
     public boolean isSymmetricHelper(TreeNode t1, TreeNode t2) {
         if (t1 == null && t2 == null) return true;
         if (t1 == null && t2 != null || t1 != null && t2 == null) return false;
-        return t1.val == t2.val && isSymmetricHelper(t1.left, t2.right) && isSymmetricHelper(t1.right, t2.left); 
+        return t1.val == t2.val 
+            && isSymmetricHelper(t1.left, t2.right) 
+            && isSymmetricHelper(t1.right, t2.left);
     }
 }
