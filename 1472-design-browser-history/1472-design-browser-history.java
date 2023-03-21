9bff510b3a8ed0e7c85510c4a534702ca6f71e4a
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2023-03-21 13:10:40 +0800

    Time: 61 ms (39.20%), Space: 50.9 MB (15.70%) - LeetHub

--- 1472-design-browser-history/1472-design-browser-history.java
+++ 1472-design-browser-history/1472-design-browser-history.java
@@ -1,32 +1,37 @@
 class BrowserHistory {
-    Stack<String> backwards;
-    Stack<String> forwards;
+    List<String> history;
+    int max, curr;
 
     public BrowserHistory(String homepage) {
-        backwards = new Stack<>();
-        forwards = new Stack<>();
-        backwards.push(homepage);
+        history = new ArrayList<>();
+        history.add(homepage);
+        max = 0;
+        curr = 0;
     }
     
     public void visit(String url) {
-        backwards.push(url); //add url to backwards stack
-        while (!forwards.isEmpty()) forwards.pop(); //clear up all forward history
+        max = curr + 1; //clear forward history
+        if (max < history.size()) history.add(max, url);
+        else history.add(url);
+        curr = max;
+        // System.out.println("add " + url + " " + history);
+        // System.out.println("curr " + curr + " max " + max);
     }
     
     public String back(int steps) {
-        while (backwards.size() > 1 && steps > 0) {
-            forwards.push(backwards.pop());
+        while (curr > 0 && steps > 0) {
+            curr--;
             steps--;
         }
-        return backwards.peek();
+        return history.get(curr);
     }
     
     public String forward(int steps) {
-        while (!forwards.isEmpty() && steps > 0) {
-            backwards.push(forwards.pop());
+        while (curr < max && steps > 0) {
+            curr++;
             steps--;
         }
-        return backwards.peek();
+        return history.get(curr);
     }
 }
 
