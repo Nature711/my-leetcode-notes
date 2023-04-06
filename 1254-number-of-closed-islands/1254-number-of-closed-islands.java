1396e59b3ace4fc6a08d3608f5789cd2324a55a6
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2023-04-06 16:17:55 +0800

    Time: 1 ms (100.00%), Space: 43.5 MB (16.44%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.java
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.java
@@ -2,28 +2,16 @@ class Solution {
     int[][] grids;
     int m, n;
     public int closedIsland(int[][] grid) {
-        m = grid.length;
-        n = grid[0].length;
         grids = grid;
         
-        // mark top and bottom boundaries
-        for (int i = 0; i < n; i++) {
-            if (grids[0][i] == 0) dfs(0, i);
-            if (grids[m-1][i] == 0) dfs(m-1, i);
-        }
-        
-        // mark left and right boundaries
-        for (int i = 0; i < m; i++) {
-            if (grids[i][0] == 0) dfs(i, 0);
-            if (grids[i][n-1] == 0) dfs(i, n-1);
-        }
-        
+        m = grid.length;
+        n = grid[0].length;
         int count = 0;
+        
         for (int i = 1; i < m - 1; i++) {
             for (int j = 1; j < n - 1; j++) {
                 if (grids[i][j] == 0) {
-                    dfs(i, j); 
-                    count++;
+                    if (dfs(i, j)) count++;
                 }
             }
         }
@@ -31,14 +19,16 @@ class Solution {
         return count;
     }
     
-    public void dfs(int i, int j) {
-        if (i < 0 || i >= m || j < 0 || j >= n || grids[i][j] != 0) return;
+    public boolean dfs(int i, int j) {
+        if (i < 0 || i >= m || j < 0 || j >= n) return false;
+        if (grids[i][j] == 1) return true;
+        grids[i][j] = 1;
         
-        grids[i][j] = 2;
+        boolean up = dfs(i - 1, j);
+        boolean down = dfs(i + 1, j);
+        boolean left = dfs(i, j - 1);
+        boolean right = dfs(i, j + 1);
         
-        dfs(i + 1, j);
-        dfs(i - 1, j);
-        dfs(i, j + 1);
-        dfs(i, j - 1);
+        return up && down && left && right;
     }
 }
