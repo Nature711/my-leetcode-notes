b5fac790dd385729fc991a7e05ff6b6099da4123
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2023-04-16 10:51:43 +0800

    Time: 3 ms (79.61%), Space: 42.8 MB (10.24%) - LeetHub

--- 0946-validate-stack-sequences/0946-validate-stack-sequences.java
+++ 0946-validate-stack-sequences/0946-validate-stack-sequences.java
@@ -1,25 +1,16 @@
 class Solution {
     public boolean validateStackSequences(int[] pushed, int[] popped) {
         Stack<Integer> stack = new Stack<>();
-        int j = 0;
-        for (int i = 0; i < pushed.length; i++) {
-            //System.out.println(" before i " + i + " " + stack + " j " + j);
+        int i = 0, j = 0, n = pushed.length;
+        
+        while (i < n) {
             if (!stack.isEmpty() && stack.peek() == popped[j]) {
-                //System.out.println("case1");
                 stack.pop();
                 j++;
-                i--;
-            } else {
-                //System.out.println("case2");
-                stack.push(pushed[i]);
-                if (stack.peek() == popped[j]) {
-                    //System.out.println("case2.1");
-                    stack.pop();
-                    j++;
-                } else { }//System.out.println("case2.2");
-                
-            }       
-            //System.out.println("after i " + i + " " + stack + " j " + j);
+                continue;
+            } 
+            stack.push(pushed[i]);
+            i++;
         }
         
         while (!stack.isEmpty()) {
