d5f0ef212f50167314ac46b2f4a8e9e724615629
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2023-06-02 10:27:05 +0800

    Time: 249 ms (20.26%), Space: 43.8 MB (17.35%) - LeetHub

---
+++ 2101-detonate-the-maximum-bombs/2101-detonate-the-maximum-bombs.java
@@ -0,0 +1,48 @@
+class Solution {
+    HashMap<Integer, List<Integer>> graph = new HashMap<>();
+    int[][] bbs;
+    public int maximumDetonation(int[][] bombs) {
+        int n = bombs.length;
+        bbs = bombs;
+        Arrays.sort(bombs, (b1, b2) -> {
+            int deltaX = b1[0] - b2[0];
+            if (deltaX != 0) return deltaX;
+            else return b1[1] - b2[1];
+        });
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j) continue;
+                double d = Math.sqrt(Math.pow(bombs[j][1] - bombs[i][1], 2) + 
+                    Math.pow(bombs[j][0] - bombs[i][0], 2));
+                if (d <= bombs[i][2]) {
+                    if (graph.containsKey(i)) graph.get(i).add(j);
+                    else {
+                        List<Integer> list = new ArrayList<>();
+                        list.add(j);
+                        graph.put(i, list);
+                    }
+                }
+                
+            }
+        }
+        int maxDetonation = 1;
+        for (int i = 0; i < n; i++) {
+            maxDetonation = Math.max(maxDetonation, dfs(i, new HashSet<>()));
+        }
+        return maxDetonation;
+    }
+    
+    public int dfs(int start, Set<Integer> visited) {
+        visited.add(start);
+        int total = 1;
+        if (graph.containsKey(start)) {
+            List<Integer> neighbors = graph.get(start);
+            for (int nei: neighbors) {
+                if (!visited.contains(nei)) total += dfs(nei, visited);
+            }
+        }
+        return total;
+    }
+    
+    
+}
