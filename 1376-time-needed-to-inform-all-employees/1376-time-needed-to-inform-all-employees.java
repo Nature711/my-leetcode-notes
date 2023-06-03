9f6f95f6b5bb1f1fd84ab92d9c56f6be91529a5f
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2023-06-03 09:37:34 +0800

    Time: 100 ms (47.48%), Space: 64.3 MB (20.60%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.java
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.java
@@ -1,38 +1,35 @@
 class Solution {
     HashMap<Integer, List<Integer>> map = new HashMap<>();
-    List<Integer> times = new ArrayList<>();
+    int maxTime = 0;
+    int[] times;
     
     public int numOfMinutes(int n, int headID, int[] manager, int[] informTime) {
-    
+        times = informTime;
         for (int i = 0; i < manager.length; i++) {
             if (manager[i] == -1) continue;
-            List<Integer> list = map.getOrDefault(manager[i], new ArrayList<>());
-            list.add(i);
-            map.put(manager[i], list);
+            if (map.containsKey(manager[i])) map.get(manager[i]).add(i);
+            else {
+                List<Integer> workers = new ArrayList<>();
+                workers.add(i);
+                map.put(manager[i], workers);
+            }
         }
         
-        int[] currTime = new int[]{informTime[headID]};
-        
-        backtrack(headID, informTime, currTime);
-        
-        int res = 0;
-        for (int time: times) res = Math.max(res, time);
-        return res;
+        backtrack(headID, 0);
+        return maxTime;
     }
     
-    public void backtrack(int root, int[] informTime, int[] currTime) {
-        
-        if (!map.containsKey(root)) {
-            times.add(currTime[0]);
+    
+    public void backtrack(int start, int cumuTime) {
+        if (!map.containsKey(start)) {
+            maxTime = Math.max(cumuTime, maxTime);
             return;
+        } 
+        
+        List<Integer> workers = map.get(start);
+        for (int worker: workers) {
+            backtrack(worker, cumuTime + times[start]);
         }
         
-        for (int sub: map.get(root)) {
-            int oldTime = currTime[0];
-            currTime[0] = oldTime + informTime[sub];
-            
-            backtrack(sub, informTime, currTime);
-            currTime[0] = oldTime;
-        }
     }
 }
