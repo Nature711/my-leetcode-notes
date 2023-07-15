3e02654dfa1e6f8d59b5588aa6d9e834d9ce1b71
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2023-07-15 09:57:49 +0800

    Time: 71 ms (61.85%), Space: 100.6 MB (46.65%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -1,16 +1,20 @@
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
-        Arrays.sort(intervals, (i1, i2) -> Integer.compare(i1[0], i2[0]));
-        int maxEnd = intervals[0][1];
-        int removeCount = 0;
-        for (int i = 1; i < intervals.length; i++) {
-            if (intervals[i][0] < maxEnd) {
-                removeCount++;
-                maxEnd = Math.min(maxEnd, intervals[i][1]);
-            } else {
-                maxEnd = intervals[i][1];
+        Arrays.sort(intervals, (i1, i2) -> (i1[0] - i2[0]));
+        
+        int minEnd = Integer.MIN_VALUE, removalCount = 0;
+        
+        for (int[] interval: intervals) {
+            if (interval[0] >= minEnd) { //no overlap
+                minEnd = interval[1]; //update minEnd
+                //no removal needed
+            } else { //overlap
+                minEnd = Math.min(interval[1], minEnd);
+                //remove the longer interval (either the one associated with current minEnd or the current interval)
+                removalCount++;
             }
         }
-        return removeCount;
+        
+        return removalCount;
     }
 }
