7c46d1275c6fa82c31d0910aa7ef1df9a9820acb
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2023-07-19 00:13:53 +0800

    Time: 67 ms (65.75%), Space: 120.6 MB (50.92%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.java
+++ 0146-lru-cache/0146-lru-cache.java
@@ -1,38 +1,99 @@
-import java.util.Map.Entry;
-
 class LRUCache {
+    HashMap<Integer, ListNode> map;
+    ListNode head, tail;
+    int cap;
 
-    private LinkedHashMap<Integer, Integer> lru;
-    private int capacity;
-    
     public LRUCache(int capacity) {
-        this.capacity = capacity;
-        lru = new LinkedHashMap<>();
+        map = new HashMap<>();
+        this.cap = capacity;
     }
     
     public int get(int key) {
-        Integer val = lru.get(key);
+ 
         
-        if (val == null) return -1;
-        else {
-            lru.remove(key);
-            lru.put(key, val);
-            return val;   
+        if (!map.containsKey(key)) {
+          
+            return -1;
         }
+            
+        ListNode node = map.get(key);
+        
+        if (node == head) {
+  
+            return head.val;
+        }
+
+        ListNode nNext = node.next, nPrev = node.prev;
+        node.next = head;
+        node.prev = null;
+        head.prev = node;
+        nPrev.next = nNext;
+        if (nNext != null) nNext.prev = nPrev;
+        head = node;
+        
+        if (node == tail) tail = nPrev;
+     
+        return node.val;
     }
     
     public void put(int key, int value) {
-        Integer val = lru.get(key);
+       
         
-        if (val == null) lru.put(key, value);
-        else {
-            lru.remove(key);
-            lru.put(key, value);
+        int res = this.get(key);
+        if (res == -1) { //add new kv pair
+            if (map.size() == cap) { //exceeds capacity, evict LRU
+                map.remove(tail.key);
+                ListNode tmp = tail.prev;
+                if (tmp != null) {
+                    tmp.next = null;
+                    tail = tmp;
+                } else {
+                    head = null;
+                    tail = null;
+                }
+            }
+            ListNode newNode = new ListNode(key, value);
+            if (head == null) {
+                head = newNode;
+                tail = head;
+            } else {
+                newNode.next = head;
+                head.prev = newNode;
+                head = newNode;
+            }
+            map.put(key, newNode);
+        } else { //the exisiting kv pair node is already moved to front (head)
+            head.val = value; //update value of existing kv pair
         }
-        
-        if (lru.size() > capacity) {
-            Entry<Integer, Integer> entry = lru.entrySet().iterator().next();
-            lru.remove(entry.getKey());
+  
+    }
+    
+    class ListNode {
+        int key;
+        int val;
+        ListNode next;
+        ListNode prev;
+        public ListNode(int key, int val) {
+            this.key = key;
+            this.val = val;
         }
     }
+    
+    public void printList() {
+        ListNode ptr = head;
+        System.out.print("list ");
+        while (ptr != null) {
+            System.out.print(ptr.key + " ");
+            ptr = ptr.next;
+        }
+        System.out.println("end ");
+    }
+    
 }
+
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * LRUCache obj = new LRUCache(capacity);
+ * int param_1 = obj.get(key);
+ * obj.put(key,value);
+ */
