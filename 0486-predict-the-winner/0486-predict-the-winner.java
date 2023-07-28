c7ad377c1b2edea4b5212e00deeb909d886f5deb
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2023-07-28 22:25:34 +0800

    Time: 1 ms (54.31%), Space: 40 MB (53.98%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,44 +1,35 @@
 class Solution {
     public boolean PredictTheWinner(int[] nums) {
-        int[] res = getScores(0, nums.length - 1, 1, nums);
-        return res[0] >= res[1];
-    }
-    
-    public int[] getScores(int start, int end, int turn, int[] nums) {
-        int[] res = new int[2];
-        
-        if (start == end) {
-            if (turn == 1) {
-                res[0] = nums[start];
-                res[1] = 0;
-            } else {
-                res[0] = 0;
-                res[1] = nums[start];
-            }
-            return res;
-        } else {
-            if (turn == 1) {
-                int[] res1 = getScores(start + 1, end, 2, nums);
-                int[] res2 = getScores(start, end - 1, 2, nums);
-                if (res1[1] < res2[1]) {
-                    res[0] = nums[start] + res1[0];
-                    res[1] = res1[1];
-                } else {
-                    res[0] = nums[end] + res2[0];
-                    res[1] = res2[1];
-                }
-            } else {
-                int[] res1 = getScores(start + 1, end, 1, nums);
-                int[] res2 = getScores(start, end - 1, 1, nums);
-                if (res1[0] < res2[0]) {
-                    res[1] = nums[start] + res1[1];
-                    res[0] = res1[0];
-                } else {
-                    res[1] = nums[end] + res2[1];
-                    res[0] = res2[0];
-                }
+        int n = nums.length;
+        int[][][] dp = new int[n][n][2];
+
+        // Base case: Subproblems with size 1
+        for (int i = 0; i < n; i++) {
+            dp[i][i][0] = nums[i]; // Player 1 score
+            dp[i][i][1] = 0;       // Player 2 score
+        }
+
+        // Filling up the DP table for subproblems with increasing size
+        for (int len = 2; len <= n; len++) {
+            for (int i = 0; i <= n - len; i++) {
+                int j = i + len - 1;
+
+                // Calculate the scores for both players for the current subarray
+                int leftPlayer1 = nums[i] + dp[i + 1][j][1];          // Player 1 picks the leftmost element
+                int leftPlayer2 = dp[i + 1][j][0];                   // Player 2 picks the leftmost element
+
+                int rightPlayer1 = nums[j] + dp[i][j - 1][1];         // Player 1 picks the rightmost element
+                int rightPlayer2 = dp[i][j - 1][0];                  // Player 2 picks the rightmost element
+
+                // Choose the best option for Player 1 and update the DP table
+                dp[i][j][0] = Math.max(leftPlayer1, rightPlayer1);
+
+                // The remaining score goes to Player 2
+                dp[i][j][1] = leftPlayer1 > rightPlayer1 ? leftPlayer2 : rightPlayer2;
             }
         }
-        return res;
+
+        // dp[0][n - 1] contains the scores for both players for the entire array
+        return dp[0][n - 1][0] >= dp[0][n - 1][1];
     }
 }
