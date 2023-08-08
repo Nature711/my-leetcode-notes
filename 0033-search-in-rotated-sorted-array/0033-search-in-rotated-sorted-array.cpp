2b5d03de3ca6b75017b7919fead5a062acf55624
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2023-08-08 12:06:05 +0800

    Time: 0 ms (100.00%), Space: 11.1 MB (13.70%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,36 +1,18 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        if (nums[0] <= nums[nums.size() - 1]) {
-            return binarySearch(nums, 0, nums.size() - 1, target);
-        } else {
-            int turn = findTurningPoint(nums);
-            //cout << "turn " << turn << endl;
-            int idx = binarySearch(nums, 0, turn, target);
-            if (idx != -1) return idx;
-            else return binarySearch(nums, turn + 1, nums.size() - 1, target);
-        }
-    }
-    
-    int binarySearch(vector<int>& nums, int low, int high, int target) {
+        int low = 0, high = nums.size() - 1;
         while (low <= high) {
             int mid = low + (high - low) / 2;
-            if (nums[mid] == target) return mid;
-            else if (nums[mid] < target) low = mid + 1;
-            else high = mid - 1;
+            if (nums[mid] == target) return mid; //target found 
+            if (nums[low] <= nums[mid]) { //left half [low...mid - 1] sorted 
+                if (nums[low] <= target && target < nums[mid]) high = mid - 1; //target exists in sorted half
+                else low = mid + 1; //target in other half
+            } else { //right half [mid + 1...high] sorted
+                if (nums[mid] < target && target <= nums[high]) low = mid + 1;
+                else high = mid - 1;
+            }
         }
         return -1;
-    }
-    
-    int findTurningPoint(vector<int>& nums) {
-        int low = 0, high = nums.size() - 1;
-        while (low < high - 1) {
-            int mid = low + (high - low) / 2;
-            //cout << "mid " << mid << endl;
-            if (nums[mid] > nums[mid + 1]) return mid;
-            else if (nums[mid] > nums[high]) low = mid + 1;
-            else high = mid;
-        }
-        return low;
     }
 };
