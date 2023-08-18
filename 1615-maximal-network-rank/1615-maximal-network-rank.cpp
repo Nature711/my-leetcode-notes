46979ebff0a0aea9e12ad24a78a24904c399a6d2
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2023-08-18 14:46:43 +0800

    Time: 104 ms (46.22%), Space: 39.2 MB (31.01%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.cpp
+++ 1615-maximal-network-rank/1615-maximal-network-rank.cpp
@@ -9,39 +9,17 @@ public:
             graph[road[1]].insert(road[0]);
         }
 
-     
         int maxRank = 0;
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
-                unordered_set<int> visited;
-                visited.insert(i);
-                if (canReach(i, j, graph, visited)) {
-                    int rankI = graph[i].size(), rankJ = graph[j].size();
-                    //cout << "i " << i << " rank "<< rankI << " j " << j << " rankJ " << rankJ << endl;
-                    int totalRank = rankI + rankJ;
-                    if (graph[i].find(j) != graph[i].end()) totalRank--;
-                    maxRank = max(maxRank, totalRank);
-                }
+                int rankI = graph[i].size(), rankJ = graph[j].size();
+                int totalRank = rankI + rankJ;
+                if (graph[i].find(j) != graph[i].end()) totalRank--;
+                maxRank = max(maxRank, totalRank);
             }
         }
-        
         return maxRank;
     }
-    
-    bool canReach(int i, int target, unordered_map<int, unordered_set<int>>& graph, unordered_set<int>& visited) {
-        return true;
-//         if (i == target) return true;
-//         if (graph.find(i) == graph.end()) return false;
-        
-//         for (int neighbor: graph[i]) {
-//             if (visited.find(neighbor) == visited.end()) {
-//                 visited.insert(neighbor);
-//                 if (canReach(neighbor, target, graph, visited)) return true;
-//             }
-//         }
-//         return false;
-    }
-    
     
     
 };
