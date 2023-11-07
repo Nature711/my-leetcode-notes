55893e091e702d7e59e6fb853ea8841bc6af22af
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2023-11-07 10:33:11 +0800

    Time: 66 ms (11.98%), Space: 56.3 MB (29.17%) - LeetHub

--- 1921-eliminate-maximum-number-of-monsters/1921-eliminate-maximum-number-of-monsters.java
+++ 1921-eliminate-maximum-number-of-monsters/1921-eliminate-maximum-number-of-monsters.java
@@ -1,17 +1,18 @@
 class Solution {
     public int eliminateMaximum(int[] dist, int[] speed) {
-        int n = dist.length;
-        PriorityQueue<Pair<Integer, Integer>> pq = new PriorityQueue<>((p, q) -> Double.compare((double) p.getKey() / p.getValue(), (double) q.getKey() / q.getValue())); 
-        for (int i = 0; i < n; i++) {
-            pq.add(new Pair<>(dist[i], speed[i]));
+        PriorityQueue<Double> pq = new PriorityQueue<>();
+        for (int i = 0; i < dist.length; i++) {
+            pq.add((double) dist[i] / speed[i]);
         }
-        int t = 0;
+        double t = 1.00;
+        int killed = 1;
+        pq.poll();
         while (!pq.isEmpty()) {
-            Pair<Integer, Integer> p = pq.poll();
-            int remDis = p.getKey() - t * p.getValue();
-            if (remDis <= 0) return n - pq.size() - 1;
+            double arrival = pq.poll();
+            if (arrival <= t) return killed;
+            killed++;
             t++;
         }
-        return n;
+        return killed;
     }
 }
