e854617746aedba4996f753e6a8887ee5fb76607
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2024-06-15 23:52:46 +0800

    Time: 0 ms (100.00%), Space: 40.8 MB (74.74%) - LeetHub

--- 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.java
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.java
@@ -14,33 +14,33 @@
  * }
  */
 class Solution {
-    
-    Stack<TreeNode> stack = new Stack<>();
-    
+    List<Integer> vals = new ArrayList<>();
+    int idx = 0;
     public TreeNode bstToGst(TreeNode root) {
         inorder(root);
+        Collections.reverse(vals);
+        int[] prefixSums = new int[vals.size() + 1];
         
-        int acc = 0;
-        while (!stack.isEmpty()) {
-            TreeNode currNode = stack.pop();
-            acc +=  currNode.val;
-            currNode.val = acc;
+        for (int i = 1; i <= vals.size(); i++) {
+            prefixSums[i] = vals.get(i - 1) + prefixSums[i - 1];
         }
         
+        inorderUpdate(root, prefixSums);
         return root;
     }
     
-    public void inorder(TreeNode node) {
-        if (node == null) return;
-        inorder(node.left);
-        stack.push(node);
-        inorder(node.right);
+    void inorder(TreeNode root) {
+        if (root == null) return;
+        inorder(root.left);
+        vals.add(root.val);
+        inorder(root.right);
     }
     
-    
-    
-    
+    void inorderUpdate(TreeNode root, int[] prefixSums) {
+        if (root == null) return;
+        inorderUpdate(root.left, prefixSums);
+        root.val = prefixSums[prefixSums.length - idx - 1];
+        idx++;
+        inorderUpdate(root.right, prefixSums);
+    }
 }
-
-
-
