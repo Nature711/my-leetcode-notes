cb4465693af5c8673d28f3035d5b5b5d3eda2985
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2024-06-21 12:06:42 +0800

    Time: 8 ms (21.92%), Space: 45.1 MB (15.09%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -8,17 +8,13 @@
  * }
  */
 class Solution {
-    
-    HashMap<Integer, Integer> nodeToLevel = new HashMap<>();
     List<List<TreeNode>> paths = new ArrayList<>();
     
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
         backtrack(root, new ArrayList<>(), p, q, 0);
         
-        int pLevel = nodeToLevel.get(p.val), qLevel = nodeToLevel.get(q.val);
-        int minLevel = Math.min(pLevel, qLevel);
-        
-        for (int i = minLevel; i >= 0; i--) {
+        int minLevel = Math.min(paths.get(0).size(), paths.get(1).size());
+        for (int i = minLevel - 1; i >= 0; i--) {
             if (paths.get(0).get(i) == paths.get(1).get(i)) return paths.get(0).get(i);
         }
         
@@ -26,9 +22,7 @@ class Solution {
     }
 
     void backtrack(TreeNode curr, List<TreeNode> currPath, TreeNode p, TreeNode q, int level) {
-            if (curr == null || paths.size() == 2) return;
-        
-            nodeToLevel.put(curr.val, level);                                                                                             
+            if (curr == null || paths.size() == 2) return;                                                                                           
             currPath.add(curr);
             if (curr == p || curr == q) {
                 paths.add(new ArrayList<>(currPath));
