96f1b8ad97356f646f89820c9c5b236fd4914f27
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2024-06-24 09:11:32 +0800

    Time: 0 ms (100.00%), Space: 47.5 MB (54.17%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.java
@@ -14,45 +14,18 @@
  * }
  */
 class Solution {
-    
-    public int countDepth(TreeNode root, boolean goLeft) {
-        int depth = 0;
-        while (root != null) {
-            root = goLeft ? root.left : root.right;
-            depth++;
-        }
-        return depth;
-    }
-
+    int count = 0;
     public int countNodes(TreeNode root) {
-        if (root == null) return 0;
+        traverse(root);
         
-        int maxDepth = countDepth(root, true);
-        int rightDepth = countDepth(root, false);
-        
-        if (maxDepth == rightDepth) return (int) Math.pow(2, maxDepth) - 1;
-        
-        int numLeaf = (int) Math.pow(2, maxDepth - 1) - 1;
-        TreeNode leftTree = root.left, rightTree = root.right;
-        int leftRightDepth = countDepth(leftTree, false), leftDepth = maxDepth - 1;
-        
-        while (leftTree != null) {  
-            if (leftDepth > leftRightDepth) {
-                rightTree = leftTree.right;
-                leftTree = leftTree.left;
-            }
-            else {
-                numLeaf += (int) Math.pow(2, leftDepth - 1);
-                if (rightTree == null) break;
-                leftTree = rightTree.left;
-                rightTree = rightTree.right;
-            }
-            leftRightDepth = countDepth(leftTree, false);
-            leftDepth--;
-        }
-        
-        return numLeaf;
+        return count;
     }
     
-    
+    void traverse(TreeNode root) {
+        if (root == null) return;
+        
+        count++;
+        traverse(root.left);
+        traverse(root.right);
+    }
 }
