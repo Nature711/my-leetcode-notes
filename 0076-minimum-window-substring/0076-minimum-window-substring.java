0d5638e2337dad835a32b2aa5e7075d1e5f06740
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2024-07-27 11:05:42 +0800

    Time: 20 ms (40.40%), Space: 45.4 MB (21.92%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,7 +1,7 @@
 class Solution {
     public String minWindow(String s, String t) {
         HashMap<Character, Integer> need = new HashMap<>();
-        HashMap<Character, Integer> have = new HashMap<>();
+        HashMap<Character, Integer> window = new HashMap<>();
         int valid = 0;
         for (char c: t.toCharArray()) {
             need.put(c, need.getOrDefault(c, 0) + 1);
@@ -10,32 +10,23 @@ class Solution {
         int startIdx = -1, minLen = Integer.MAX_VALUE;
         
         while (right < s.length()) {
-            // expanding window
-            char rightChar = s.charAt(right);
-            if (need.containsKey(rightChar)) {
-                have.put(rightChar, have.getOrDefault(rightChar, 0) + 1); 
-                if (have.get(rightChar).equals(need.get(rightChar))) valid++;
-            }
+            char c = s.charAt(right);
+            right++;
+            window.put(c, window.getOrDefault(c, 0) + 1);
+            if (window.get(c).equals(need.get(c))) valid++;
             
-            // shrinking window
             while (valid == need.size()) {
-                int currLen = right - left + 1;
+                int currLen = right - left;
                 if (currLen < minLen) {
                     minLen = currLen;
                     startIdx = left;
                 }
                 
-                char leftChar = s.charAt(left);
-                if (need.containsKey(leftChar)) {
-                    int rem = have.get(leftChar) - 1;
-                    if (rem < need.get(leftChar)) valid--;
-                    
-                    if (rem == 0) have.remove(leftChar);
-                    else have.put(leftChar, rem);
-                }
+                char cc = s.charAt(left);
                 left++;
+                window.put(cc, window.get(cc) - 1); // window.get() won't throw error
+                if (need.containsKey(cc) && window.get(cc) < need.get(cc)) valid--;
             }
-            right++;
         }
         
         if (startIdx == -1) return "";
