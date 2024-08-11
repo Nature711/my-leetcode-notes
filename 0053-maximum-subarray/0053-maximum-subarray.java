158b8b291f5e64a72c9b55e90b146af33b2b3d0e
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2024-08-11 16:25:45 +0800

    Time: 1 ms (99.53%), Space: 57.1 MB (41.05%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,32 +1,10 @@
 class Solution {
-    
-    int[] globalNums;
     public int maxSubArray(int[] nums) {
-        globalNums = nums;
-        return helper(0, nums.length - 1);
-    }
-    
-    public int helper(int low, int high) {
-        if (low == high) return globalNums[low];
-        
-        int mid = low + (high - low) / 2;
-        int leftMax = helper(low, mid);
-        int rightMax = helper(mid + 1, high);
-        
-        int currSumLeft = 0, maxFromLeft = 0, currSumRight = 0, maxFromRight = 0;
-        
-        for (int i = mid - 1; i >= low; i--) {
-            currSumLeft += globalNums[i];
-            maxFromLeft = Math.max(maxFromLeft, currSumLeft);
+        int max = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] = Math.max(nums[i] + nums[i - 1], nums[i]);
+            max = Math.max(max, nums[i]);
         }
-        for (int i = mid + 1; i <= high; i++) {
-            currSumRight += globalNums[i];
-            maxFromRight = Math.max(maxFromRight, currSumRight);
-        }
-        
-        int middleMax = globalNums[mid] + maxFromLeft + maxFromRight;
-    
-        return Math.max(leftMax, Math.max(rightMax, middleMax));
+        return max;
     }
-    
 }
