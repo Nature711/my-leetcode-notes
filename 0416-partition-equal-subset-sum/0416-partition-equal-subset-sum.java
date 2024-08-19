2f98130dd24cfd80cf7ae400453b01f003ae9f78
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2024-08-19 22:56:18 +0800

    Time: 36 ms (72.19%), Space: 46.6 MB (58.33%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,27 +1,24 @@
 class Solution {
-
     public boolean canPartition(int[] nums) {
-
-        int n = nums.length;
         int sum = 0;
         for (int num: nums) sum += num;
-        if (sum % 2 == 1) return false;
+        if (sum % 2 != 0) return false;
         int target = sum / 2;
-        
-        boolean[][] dp = new boolean[n + 1][target + 1]; 
-        //dp[i][j] = whether we can form a subset sum of j using elements from 0...i
-        for (int i = 0; i <= target; i++) dp[0][i] = false;
-        for (int i = 0; i <= n; i++) dp[i][0] = true;
-        
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= target; j++) {
-                int rem = j - nums[i - 1];
-                dp[i][j] = dp[i - 1][j] || (rem < 0 ? false : dp[i - 1][rem]);
-       
-            }
-        }
-  
-        return dp[n][target];
-    }
+        int n = nums.length;
+        boolean[][] dp = new boolean[n][target + 1]; // dp[i][j]: true if we can use nums from [0...i] to make up j
+        for (int i = 0; i < n; i++) dp[i][0] = true; // can always make up 0 by using anything
+        for (int i = 1; i <= target; i++) dp[0][i] = (nums[0] == i);
     
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j <= target; j++) {
+                dp[i][j] = dp[i - 1][j];
+                if (j - nums[i] >= 0) {
+                    dp[i][j] = dp[i - 1][j - nums[i]] || dp[i - 1][j];
+                }
+            }
+            if (dp[i][target]) return true;
+        }
+        
+        return dp[n - 1][target];
+    }
 }
