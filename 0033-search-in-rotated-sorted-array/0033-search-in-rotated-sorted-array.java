38ada770a38de1c44d258dc237dd69739eb129cf
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2024-09-14 10:26:15 +0800

    Time: 0 ms (100.00%), Space: 41.3 MB (99.65%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,49 +1,24 @@
 class Solution {
     public int search(int[] nums, int target) {
-        
-        int minIndex = findMinIndex(nums);
-        
-        int low, high;
-        
-        if (minIndex == 0) {
-            low = 0;
-            high = nums.length - 1;
-        } else if (nums[0] <= target && target <= nums[minIndex - 1]) {
-            low = 0;
-            high = minIndex - 1;
-        } else {
-            low = minIndex;
-            high = nums.length - 1;
-        }
-        
-        //0 ... minIndex - 1 && minIndex ... high is sorted
-        
-        while (low <= high) {
-            int mid = low + (high - low) / 2;
-            if (nums[mid] == target) return mid;
-            else if (nums[mid] < target) low = mid + 1;
-            else high = mid - 1;
-        }
-        return -1;
-        
-    }
-    
-    public int findMinIndex(int[] nums) {
         int low = 0, high = nums.length - 1;
-        if (low == high || nums[low] < nums[high]) return 0;
-    
         while (low < high) {
             int mid = low + (high - low) / 2;
-            if (nums[mid] > nums[mid + 1]) return mid + 1;
-            if (nums[low] <= nums[mid]) {
-                //left half sorted; turning point in right half
-                low = mid;
-            } else {
-                //right half sorted; turning point in left half
-                high = mid;
+            if (nums[mid] == target) return mid;
+            if (nums[0] <= nums[mid]) { // left part sorted
+                if ((nums[0] <= target) && (target < nums[mid])) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            } else { // right part sorted
+                if ((nums[mid] < target) && (target <= nums[high])) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
             }
         }
+        if (nums[low] == target) return low;
         return -1;
     }
-  
 }
