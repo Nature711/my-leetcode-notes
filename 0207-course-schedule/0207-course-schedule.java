ff5dab70f98324d286a9a78a30c145a33033c7f0
Hu Tianran <77217430+Nature711@users.noreply.github.com>
2024-10-20 11:02:57 +0800

    Time: 9 ms (23.97%), Space: 46.1 MB (10.48%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,45 +1,41 @@
 class Solution {
-    
-    HashMap<Integer, List<Integer>> map = new HashMap<>();
-    int[] status; //0 for not visited, 1 for visting, 2 for visited
-    
+    HashMap<Integer, List<Integer>> graph = new HashMap<>();
+    HashMap<Integer, Integer> status = new HashMap<>();
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        status = new int[numCourses];
-        
-        for (int[] pre: prerequisites) { //construct prerequiste map
-            if (map.containsKey(pre[0])) map.get(pre[0]).add(pre[1]);
-            else {
-                List<Integer> list = new ArrayList<>();
-                list.add(pre[1]);
-                map.put(pre[0], list);
-            }
+        for (int[] pre: prerequisites) {
+            List<Integer> list = graph.getOrDefault(pre[0], new ArrayList<>());
+            list.add(pre[1]);
+            graph.put(pre[0], list);
         }
+        
+        for (int i = 0; i < numCourses; i++) status.put(i, 0);
         
         for (int i = 0; i < numCourses; i++) {
-            if (status[i] == 0) {
-                if (!dfs(i)) return false;
+            if (status.get(i) == 0) {
+                boolean res = dfs(i);
+                if (!res) return false;
             }
         }
         
         return true;
-        
     }
-   
-    public boolean dfs(int start) {
-        if (status[start] == 2) return true;
-        if (status[start] == 1) return false;
+    
+    
+    boolean dfs(int src) {
+        if (status.get(src) == 2) return true;
+        if (status.get(src) == 1) return false;
+        status.put(src, 1);
         
-        status[start] = 1;
-        
-        
-        if (map.containsKey(start)) {
-            List<Integer> prerequisites = map.get(start);
-            for (int pre: prerequisites) {
-                if (!dfs(pre)) return false;
+        if (graph.containsKey(src)) {
+            List<Integer> neighbors = graph.get(src);
+            for (int neighbor: neighbors) {
+                boolean res = dfs(neighbor);
+                if (!res) return false;
             }
         }
         
-        status[start] = 2;
+        status.put(src, 2);
+                 
         return true;
     }
-} 
+}
